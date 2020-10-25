3f1aeb5df92b03bb9410dd32625122ab8a5df57a
Mapopo (Evans) <mppdgreat@gmail.com>
2020-10-26 00:57:18 +0100

    implement signup controller

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import Handler from '../utils/middleware/Handler';
+import Auth from '../utils/helpers/Auth';
+import { TOKEN_EXPIRES_IN } from '../utils/constants';
 import User from '../models/User';
 
 /**
@@ -27,17 +29,18 @@ class AuthController {
       defaults: { firstName, lastName, email, hash },
     })
       .then(([user, created]) => {
-        if (!created) Handler.throw(res, 'Email already exist', 400);
+        if (!created) Handler.throw(res, 'Email already exist', 409);
         const payload = { id: user.id, email: user.email }
-        // const token = tokenHelper.generateToken(payload, '3d');
+        const token = Auth.generateToken(payload, TOKEN_EXPIRES_IN);
         res.status(201).json({
           data: {
             ...payload,
             firstName: user.firstName,
             lastName: user.lastName,
-            // token
+            token
           },
           message: 'Sign up successful',
+          success: true
         });
 
       }).catch(next);
@@ -49,7 +52,7 @@ class AuthController {
     next: NextFunction
     ): any {
 
-    res.end('login into your account')
+    res.end('login into your account');
   }
 }
 
---
+++ src/utils/constants.ts
@@ -0,0 +1,1 @@
+export const TOKEN_EXPIRES_IN = '3d';
